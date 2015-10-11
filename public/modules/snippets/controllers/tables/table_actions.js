f0def57ef4f07a9d06606631d0ab527cdf4a33b9
raizman2012 <raizman_leonid@hotmail.com>
2015-10-11 10:15:23 +0300

    more staff

--- public/modules/snippets/controllers/tables/table_actions.js
+++ public/modules/snippets/controllers/tables/table_actions.js
@@ -10,6 +10,7 @@ angular.module('snippets').controller('TableActionsController', ['$scope', 'clie
         $scope.customers = clientMockData.customers;
 
         $scope.reloaded = Date.now();
+
         $scope.reload = function() {
             $scope.reloaded = Date.now();
         }
